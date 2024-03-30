bbb64a607f3492976544c23843fd438777a381e4
Paulo Campos <paulo4-0@hotmail.com>
2024-03-30 11:50:01 -0300

    save note on localStorage

--- src/app.tsx
+++ src/app.tsx
@@ -9,7 +9,14 @@ interface Note {
 }
 
 export function App() {
-  const [notes, setNotes] = useState<Note[]>([])
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+    return []
+  })
 
   function onNoteCreated(content: string) {
     const newNote = {
@@ -18,7 +25,11 @@ export function App() {
       content,
     }
 
-    setNotes([newNote, ...notes])
+    const notesArray = [newNote, ...notes]
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
   return (
